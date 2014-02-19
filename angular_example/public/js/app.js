5d259f7e78834fec74e679536a4ecbe3ae45c1ad
Alexander Kahoun <alex.kahoun@outlook.com>
2014-02-18 20:54:38 -0700

    fixed routing

--- angular_example/public/js/app.js
+++ angular_example/public/js/app.js
@@ -3,9 +3,11 @@
 var todoapp = angular.module('todoapp', ['ngRoute']).config(
   function ($routeProvider) {
     $routeProvider.when('/', {
-      controller: 'TodoController'
+      controller: 'TodoController',
+      templateUrl: 'todoapp.html'
     }).when('/:status', {
-      controller: 'TodoController'
+      controller: 'TodoController',
+      templateUrl: 'todoapp.html'
     }).otherwise({
       redirectTo: '/'
     });
